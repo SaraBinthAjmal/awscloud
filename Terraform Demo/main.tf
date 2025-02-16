5bf119f82a11a59f33b8ddd5e286da99ae9585f1
Ajmal <aju.core@gmail.com>
2025-02-17 01:20:59 +0530

    update

--- Terraform Demo/main.tf
+++ Terraform Demo/main.tf
@@ -2,15 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "test" {
-  # ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
-  ami           = "ami-085ad6ae776d8f09c" #Amazon Linux 2023 AMI
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Grafana-Server"
-  }
-}
 # Addding Security Group here
 
 resource "aws_security_group" "example_sg" {
@@ -35,5 +26,22 @@ resource "aws_security_group" "example_sg" {
 
   tags = {
     Name = "grafana-sg"
+  }
+}
+
+resource "aws_subnet" "custom_subnet" {
+  vpc_id                  = vpc-0424bd07b2433d81c
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+# AWS instance here 
+resource "aws_instance" "test" {
+  # ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
+  ami           = "ami-085ad6ae776d8f09c" #Amazon Linux 2023 AMI
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Grafana-Server"
   }
 }
